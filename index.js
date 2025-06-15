ec55664715df87d7360ddf722c019ecf7c8ae638
Nishanth-G-03 <nishanthpetroleum@gmail.com>
2025-06-15 09:39:23 +0530

    Update index.js
    Added friendly route.

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Contact form backend running");
+});
+
 app.post('/send', async (req, res) => {
   const { name, email, message } = req.body;
 
